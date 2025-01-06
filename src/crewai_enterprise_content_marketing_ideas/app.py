40d0364097bf17c19f90dce4266a23f5aaa5361b
Frederic Laurin <frederic.3.laurin@gmail.com>
2025-01-06 23:03:38 +0700

    Update app.py

--- src/crewai_enterprise_content_marketing_ideas/app.py
+++ src/crewai_enterprise_content_marketing_ideas/app.py
@@ -2,15 +2,23 @@ import streamlit as st
 import sys
 import sqlite3
 
-# Specify the path to your module
-module_path = "C:/Users/Frederic Laurin/PycharmProjects/crewAI-enterprise-content-marketing-ideas-template-main/src"
 
-# Check if the path is already in sys.path
-if module_path not in sys.path:
-    sys.path.append(module_path)  # Add the path to sys.path
+import chromadb
+client = chromadb.Client()
+collection = client.get_collection(name="chroma_docs")
+results = collection.get(ids=["page"])["documents"]
+print(results) # Not found []
 
-# Verify if the path was added (optional)
-print(module_path in sys.path)  # This should print True if added successfully
+
+# # Specify the path to your module
+# module_path = "C:/Users/Frederic Laurin/PycharmProjects/crewAI-enterprise-content-marketing-ideas-template-main/src"
+
+# # Check if the path is already in sys.path
+# if module_path not in sys.path:
+#     sys.path.append(module_path)  # Add the path to sys.path
+
+# # Verify if the path was added (optional)
+# print(module_path in sys.path)  # This should print True if added successfully
 
 from crewai_enterprise_content_marketing_ideas.crew import CrewaiEnterpriseContentMarketingCrew
 
