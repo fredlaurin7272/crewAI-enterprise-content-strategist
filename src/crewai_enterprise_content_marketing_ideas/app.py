ef7974d389df85237f0a74eee46c6ffed08bac9a
Frederic Laurin <frederic.3.laurin@gmail.com>
2025-01-06 22:48:26 +0700

    adding requirements

--- src/crewai_enterprise_content_marketing_ideas/app.py
+++ src/crewai_enterprise_content_marketing_ideas/app.py
@@ -1,4 +1,17 @@
 import streamlit as st
+import sys
+import sqlite3
+
+# Specify the path to your module
+module_path = "C:/Users/Frederic Laurin/PycharmProjects/crewAI-enterprise-content-marketing-ideas-template-main/src"
+
+# Check if the path is already in sys.path
+if module_path not in sys.path:
+    sys.path.append(module_path)  # Add the path to sys.path
+
+# Verify if the path was added (optional)
+print(module_path in sys.path)  # This should print True if added successfully
+
 from crewai_enterprise_content_marketing_ideas.crew import CrewaiEnterpriseContentMarketingCrew
 
 st.title("Creative Content Generator for Mila")
